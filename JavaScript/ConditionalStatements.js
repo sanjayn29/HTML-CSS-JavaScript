353b426b9124a8da07986ae120815b1474434cfc
Sanjay N <sanjayn29.aiml@gmail.com>
2026-05-08 09:12:35 +0530

    control statements

---
+++ JavaScript/ConditionalStatements.js
@@ -0,0 +1,26 @@
+//if statement
+let age = 20;
+
+if (age >= 18) {
+  console.log("Eligible to vote");
+} 
+
+//if-else statement
+let agee = 16;
+
+if (agee >= 18) {
+  console.log("Adult");
+} else {
+  console.log("Minor");
+}
+
+//else if Ladder
+let marks = 85;
+
+if (marks >= 90) {
+  console.log("A Grade");
+} else if (marks >= 75) {
+  console.log("B Grade");
+} else {
+  console.log("C Grade");
+}
