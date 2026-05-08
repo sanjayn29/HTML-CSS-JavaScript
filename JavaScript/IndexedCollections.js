8258ac72ab2dc4306b5e4cf788b0a830c52d558f
Sanjay N <sanjayn29.aiml@gmail.com>
2026-05-08 08:08:26 +0530

    Indexed Collection : Array

---
+++ JavaScript/IndexedCollections.js
@@ -0,0 +1,20 @@
+//Indexd Colllection : Array
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log(arr[0]);
+
+arr.push("Sanjay");
+arr.push(0.25);
+arr.push(true);
+console.log(arr);
+console.log(arr.length);
+arr.pop();
+console.log(arr);
+
+for(let i =0;i < arr.length ; i++){
+    console.log(arr[i]);
+}
+
+arr.forEach(function(i){
+    console.log(i);
+});
